f235a6e6c80789e3a1a63123745a4dc82868f550
Andrew <andrew.goncharenko@mev.com>
2016-04-20 11:43:48 +0300

    changes in index

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,43 @@
 var expect = require('chai').expect;
-var zapptitude = require('./index');
+var index = require('./index');
 
-describe('zaptitude', function() {
-    it('should work!', function() {
-        expect(true).to.be.true;
+describe('Zapptitude:', function() {
+    describe('logEvent', function() {
+        it('should be no errors', function () {
+            index.logEvent("test_event");
+        });
     });
+    // describe('logBeginTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logBeginTask("test_task", "test_context");
+    //     });
+    // });
+    // describe('logSolveBinaryTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logSolveBinaryTask("binary", "test_task", "test_context", "test_topics", true, false);
+    //     });
+    // });
+    // describe('logSolveIntTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logSolveIntTask("int", "test_task", "test_context", "test_topics", 1, 0);
+    //     });
+    // });
+    // describe('logSolveFloatTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logSolveFloatTask("float", "test_task", "test_context", "test_topics", 1.2, 0.6);
+    //     });
+    // });
+    // describe('logSolveMCTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logSolveMCTask("mc", "test_task", "test_context", "test_topics", 'a', 'a', 10);
+    //     });
+    // });
+    // describe('logSolveGradTask', function() {
+    //     it('should be no errors', function () {
+    //         index.logSolveGradTask("grad", "test_task", "test_context", "test_topics", 1, 2, 10);
+    //     });
+    // });
+});
+
+describe('ZappEventManager:', function() {
 });
